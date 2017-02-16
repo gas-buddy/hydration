6f4cfb4970f34dde59652f35ea90ccf924480372
Max Metral <max@pyralis.com>
2017-02-16 17:38:17 -0500

    Avoid transpiler bug

--- src/index.js
+++ src/index.js
@@ -34,15 +34,15 @@ async function buildObject(context, name, config, tree) {
   return ClassConstructor;
 }
 
-function hydrateRecursive(context, name, config, tree, target) {
+function hydrateRecursive(context, name, config, tree, propertyTarget) {
   if (Array.isArray(config)) {
     for (const sub of config) {
-      hydrateRecursive(context, name, sub, tree, target);
+      hydrateRecursive(context, name, sub, tree, propertyTarget);
     }
   } else if (typeof config === 'object' && !Array.isArray(config)) {
     for (const [key, subConfig] of Object.entries(config)) {
       if (subConfig.module) {
-        const buildResult = buildObject(context, key, subConfig, tree, target);
+        const buildResult = buildObject(context, key, subConfig, tree, propertyTarget);
         if (buildResult) {
           // Initialize in parallel by just pushing a promise and fixing the values when done
           const valuePromise = Promise.resolve(buildResult)
@@ -52,24 +52,24 @@ function hydrateRecursive(context, name, config, tree, target) {
                 context.externalContext.logger.info('Completed hydration', { key });
               }
               tree[key] = value;
-              if (target) {
-                target[key] = value;
+              if (propertyTarget) {
+                propertyTarget[key] = value;
               }
               return value;
             });
           // Nobody is done until we are all done
           context.promise = context.promise.then(() => valuePromise);
           tree[key] = valuePromise;
-          if (target) {
-            target[key] = valuePromise;
+          if (propertyTarget) {
+            propertyTarget[key] = valuePromise;
           }
         }
       } else {
         tree[key] = {};
-        if (target) {
-          target[key] = {};
+        if (propertyTarget) {
+          propertyTarget[key] = {};
         }
-        hydrateRecursive(context, key, subConfig, tree[key], target ? target[key] : null);
+        hydrateRecursive(context, key, subConfig, tree[key], propertyTarget ? propertyTarget[key] : null);
       }
     }
   } else {
